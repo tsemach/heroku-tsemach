9c8d172e600587dbe2d4beeb74a3b23811b95944
tsemach <tsemach.mizrachi>
2019-08-08 18:25:41 +0300

    fix index.js

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,16 +1,9 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv = require("dotenv");
-dotenv.config();
-const express_1 = require("./express");
-const mongoose = require("mongoose");
-require("./application/routes");
-const uri = "mongodb+srv://tsemach:LgA4VfbH0knDwcPh@center-1-kivdh.mongodb.net?retryWrites=true&w=majority";
-mongoose.set('useFindAndModify', false);
-mongoose.connect(uri, { dbName: 'intel-todo' });
-mongoose.connection.once('open', () => {
-    console.log('connected to todos database');
+const http = require('http');
+
+var server = http.createServer((req, res) => {
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.end("Hello World");
 });
-const port = process.env.PORT || 8080;
-express_1.default.instance.listen('localhost', +port);
-//# sourceMappingURL=index.js.map
+
+let port = 8080;
+server.listen(port);
