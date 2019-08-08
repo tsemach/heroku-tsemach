07a76cd2acd8775872b0bcc290fcc1d86994e77a
tsemach <tsemach.mizrachi>
2019-08-08 18:30:38 +0300

    fix index.js

--- dist/src/index.js
+++ dist/src/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 var server = http.createServer((req, res) => {
 	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.end("Hello World");
+	res.end("Hello World from dist/src/index.js");
 });
 
 let port = process.env.PORT;
