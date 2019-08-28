caa8c473d562f83e6ed7c07b0094c5400db023b2
tsemach <tsemach.mizrachi>
2019-08-28 10:25:16 +0300

    set react-css

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-var server = http.createServer((req, res) => {
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.end("Hello World");
-});
-
-let port = process.env.PORT;
-server.listen(port);
