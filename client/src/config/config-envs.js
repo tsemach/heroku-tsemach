7712877bc4416b49db00716ba743f2a94132cb37
tsemach <tsemach.mizrachi>
2019-09-02 13:36:52 +0300

    deploy

--- client/src/config/config-envs.js
+++ client/src/config/config-envs.js
@@ -1,17 +1,17 @@
 
 
-console.log("process.env.NODE_ENV  = ", process.env.NODE_ENV);
+console.log("process.env.NODE_ENV =", process.env.NODE_ENV);
 
 let HERUKO_PORT = 8080;
 
 if (process.env.NODE_ENV === 'development') {
   HERUKO_PORT = process.env.DEV_HERUKO_PORT;
-  console.log("process.env.DEV_HERUKO_URL  = ", process.env.DEV_HERUKO_HOST + ":" + HERUKO_PORT + '/heruko',);
+  console.log("process.env.DEV_HERUKO_URL =", process.env.DEV_HERUKO_HOST + ":" + HERUKO_PORT + '/heruko',);
 }
 
 if (process.env.NODE_ENV === 'production') {
   HERUKO_PORT = process.env.PORT;
-  console.log("process.env.PRD_HERUKO_URL  = ", process.env.PRD_HERUKO_HOST + ":" + HERUKO_PORT + '/heruko',);
+  console.log("process.env.PRD_HERUKO_URL =", process.env.PRD_HERUKO_HOST + ":" + HERUKO_PORT + '/heruko',);
 }
 
 export default {  
